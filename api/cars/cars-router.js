e0c765d7fb02cd08700d0131734655a1c2f4043a
MurrayWarnock <murray.warnock@gmail.com>
2021-08-14 15:06:51 -0600

    scaffold includes model

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,22 +1,40 @@
 // DO YOUR MAGIC
 const express = require('express');
+const Car = require('./cars-model');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars');
+    try {
+        const cars = await Car.getAll();
+        res.status(200).json(cars);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.get('/:id', async (req, res, next) => {
+try {
     res.json(`getting a car with id ${req.params.id}`);
+} catch (error) {
+    next(error);    
+}    
 });
 
 router.post('/', async (req, res, next) => {
-    res.json('posting a new car');
+    try {
+        res.json('posting a new car');
+    } catch (error) {
+        next(error);    
+    }
 });
 
-// router.verb('/', async (req, res, next) => {
-//     res.json('create a new car');
-// });
+router.put('/', async (req, res, next) => {
+    try {
+        res.json('editing a new car');
+    } catch (error) {
+        next(error);    
+    }
+});
 
 module.exports = router;
 
