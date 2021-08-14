64224d95a76b5005940d578a1935d7a6c3e1d0e0
MurrayWarnock <murray.warnock@gmail.com>
2021-08-14 12:28:08 -0600

    make table

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars = [
+    {
+        vin: "111111111111111",
+        make: "toyota",
+        model: "prius",
+        mileage: 25009,
+        title: "clean",
+        transmission: "manual"
+    },{
+        vin: "111111111111111",
+        make: "ford",
+        model: "focus",
+        mileage: 25009,
+        title: "clean",
+    },{
+        vin: "111111111111111",
+        make: "volkswagon",
+        model: "bug",
+        mileage: 25009,
+    },
+];
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars);
+//     });
+// };
+
+//OR USE async/await
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate();
+    await knex('cars').insert(cars);
+};
