40e2a8433032c06db7d8d19450563845475dae35
MurrayWarnock <murray.warnock@gmail.com>
2021-08-14 14:49:32 -0600

    server/db/endpoints  scaffold

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,25 @@
 // DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    res.json('getting all cars');
+});
+
+router.get('/:id', async (req, res, next) => {
+    res.json(`getting a car with id ${req.params.id}`);
+});
+
+router.post('/', async (req, res, next) => {
+    res.json('posting a new car');
+});
+
+// router.verb('/', async (req, res, next) => {
+//     res.json('create a new car');
+// });
+
+module.exports = router;
+
+
+
+
